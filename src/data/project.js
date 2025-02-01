69e361646ba54583c311bd73aa728ff01f283f62
Naitik Tiwari <144876588+N4171k@users.noreply.github.com>
2025-02-02 00:47:43 +0530

    Update project.js

--- src/data/project.js
+++ src/data/project.js
@@ -8,6 +8,15 @@ const projects = [
       " Intelligent study planner using Gemini-Pro AI and Streamlit, featuring adaptive learning schedules, PDF content analysis, and textbook recommendations. The system generates personalized study plans based on syllabus complexity, available time, and exam dates, while its PDF parser extracts key concepts and creates practice questions from uploaded materials. Integrated with a bookstore API for resource suggestions, this tool reduces study planning time by 40% through AI-optimized scheduling.",
     link: "https://github.com/N4171k/DOST",
   },
+
+  {
+    name: "Saarthi",
+    img: project,
+    description:
+      "The AI Travel Itinerary Planner is an innovative web application built using React & Node, designed to simplify travel planning by generating personalized itineraries powered by Google's Gemini AI. Users can input their destination, trip duration, and interests (e.g., culture, food, adventure) to receive a detailed, day-by-day travel plan complete with activity recommendations and time estimates. ",
+    link: "https://saarthi.naitiktiwari.in",
+  },
+];
   {
     name: "CodePen Clone",
     img: project,
@@ -22,13 +31,4 @@ const projects = [
       "The Hospital Management System (HMS) project is a comprehensive C++ application designed to streamline hospital operations by managing patient records, doctor schedules, and administrative tasks. The system features role-based access, allowing patients to view their medical records and check doctor availability, doctors to update patient observations and set their availability, and management to create and manage user accounts",
     link: "https://github.com/N4171k/HospitalManagementSystem",
   },
-  {
-    name: "Safar Saathi",
-    img: project,
-    description:
-      "The AI Travel Itinerary Planner is an innovative web application built using Python and Streamlit, designed to simplify travel planning by generating personalized itineraries powered by Google's Gemini AI. Users can input their destination, trip duration, and interests (e.g., culture, food, adventure) to receive a detailed, day-by-day travel plan complete with activity recommendations and time estimates. ",
-    link: "https://github.com/N4171k/Safar-saathi",
-  },
-];
-
 export { projects };
